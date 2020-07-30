2c496fe5cd6dd5eb6cc0524d18577365a2c48d74
GaboDotCodes <gabedu2000@gmail.com>
2020-07-29 23:57:14 -0500

    Datos paciente 1

--- analyzer.sh
+++ analyzer.sh
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-analyzer('/home/andresagudelo/Documentos/OCTAVEproyects/PATOLOGIAS/enProceso/HUCV/patologia_AURA219/paciente_AURA219', [2,3,5,8,9,10,25,29,59,310])
-=======
-analyzer('/Users/johnalexandergaleano/Documents/aura/procesos/entrada/01077',[1 2 3 5 9 10])
->>>>>>> b418a482f615d516271ba09c035059b5dd3fc107
+analyzer('./paciente_grupoA_1', [2,5])
