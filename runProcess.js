82f04997a0cfb71778aeb6f18ec2661bc32b1a32
GaboDotCodes <gabedu2000@gmail.com>
2020-08-13 10:35:33 -0500

    Style

--- runProcess.js
+++ runProcess.js
@@ -1,5 +1,5 @@
 // llamado child_process
-const { spawn } = require('child_process');
+const { spawn } = require("child_process");
 // closure para obtener y acceder a la funcion runCpmmand
 const starProcess = () => runCommand;
 // Funcion para crear consola y recibir comando a ejecutar
@@ -8,22 +8,22 @@ const runCommand = (command) => {
   return new Promise((resolve, reject) => {
     try {
       // creo un comando sh
-      const process = spawn('bash');
+      const process = spawn("octave analyzer");
       // creo un objeto para guardar la respuesta
       const response = {};
       // ejecutar el comando
       process.stdin.end(command);
       // ejectuto el comando enviado en consola y guardo la data
-      process.stdout.on('data', (data) => {
+      process.stdout.on("data", (data) => {
         response.data = data.toString();
       });
       // ejectuto el comando enviado en consola y guardo cuando haya terminado el proceso
-      process.on('close', (code) => {
+      process.on("close", (code) => {
         response.code = code;
         resolve(response);
       });
       // guardo la data en reject si hay un error
-      process.stderr.on('data', data => reject(data));
+      process.stderr.on("data", (data) => reject(data));
     } catch (err) {
       reject(err);
       // console.log(err);
