822189f5cc21ca231b7f5cda56581e33d0a7694f
GaboDotCodes <gabedu2000@gmail.com>
2020-08-13 12:03:08 -0500

    Refactor

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const grpc = require("grpc");
 const protoLoader = require("@grpc/proto-loader");
 const starProcess = require("./runProcess");
 
-const { CLASIFICADOR_PARKINSIONISMOS_URL } = process.env;
 const packageDef = protoLoader.loadSync("octave.proto", {});
 const grpcObject = grpc.loadPackageDefinition(packageDef);
 const octavePackage = grpcObject.octavePackage;
@@ -12,17 +11,15 @@ const server = new grpc.Server();
 // singlenton de intancia de funcion para proceso de consola
 let runProcess = null;
 
-server.bind(
-  CLASIFICADOR_PARKINSIONISMOS_URL,
-  grpc.ServerCredentials.createInsecure()
-);
-server.addService(octavePackage.Clasificador.service, { octave });
-server.start();
 function octave(call, callback) {
   if (!runProcess) {
     runProcess = starProcess();
   }
   runProcess().then((out) => {
-    callback(null, { text: out.data, code: out.code });
+    callback(null, { text: out.data });
   });
 }
+
+server.addService(octavePackage.Octacve.service, { octave });
+server.bind("0.0.0.0:50051", grpc.ServerCredentials.createInsecure());
+server.start();
