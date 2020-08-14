7ba6325c2a21967c6ea30acfafdf15cd02aa270e
GaboDotCodes <gabedu2000@gmail.com>
2020-08-14 11:30:27 -0500

    Refactor

--- server.js
+++ server.js
@@ -1,25 +1,16 @@
-require("dotenv").config();
 const grpc = require("grpc");
 const protoLoader = require("@grpc/proto-loader");
-const starProcess = require("./runProcess");
 
-const packageDef = protoLoader.loadSync("octave.proto", {});
-const grpcObject = grpc.loadPackageDefinition(packageDef);
-const octavePackage = grpcObject.octavePackage;
+const packageDef = protoLoader.loadSync("octave.proto");
+const octavePackage = grpc.loadPackageDefinition(packageDef).octavePackage;
 const server = new grpc.Server();
 
-// singlenton de intancia de funcion para proceso de consola
-let runProcess = null;
+const octave = (call, callback) => {
+  console.log(call.request.tests[0].test);
+  console.log(call.request.tests[0].data.table);
+  callback(null, { text: "Ready" });
+};
 
-function octave(call, callback) {
-  if (!runProcess) {
-    runProcess = starProcess();
-  }
-  runProcess().then((out) => {
-    callback(null, { text: out.data });
-  });
-}
-
-server.addService(octavePackage.Octacve.service, { octave });
+server.addService(octavePackage.Octave.service, { octave });
 server.bind("0.0.0.0:50051", grpc.ServerCredentials.createInsecure());
 server.start();
