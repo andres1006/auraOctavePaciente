b2ffe9632a111a70713b35678a9ebcec56e7f5f4
GaboDotCodes <gabedu2000@gmail.com>
2020-08-14 11:29:52 -0500

    Refactor Client

--- client.js
+++ client.js
@@ -1,27 +1,53 @@
-const grpc = require('grpc');
-require('dotenv').config();
-const protoLoader = require('@grpc/proto-loader');
+const grpc = require("grpc");
+const protoLoader = require("@grpc/proto-loader");
 
-const { CLASIFICADOR_PARKINSIONISMOS_URL } = process.env;
-const packageDef = protoLoader.loadSync('clasificador.proto', {});
-const grpcObject = grpc.loadPackageDefinition(packageDef);
-const clasificadorPackage = grpcObject.clasificadorPackage;
-const text = process.argv[2];
+const packageDef = protoLoader.loadSync("octave.proto");
+const octavePackage = grpc.loadPackageDefinition(packageDef).octavePackage;
 
-const client = new clasificadorPackage.Clasificador(
-  CLASIFICADOR_PARKINSIONISMOS_URL,
+const client = new octavePackage.Octave(
+  "localhost:50051",
   grpc.credentials.createInsecure()
 );
 
-client.clasificador(
-  {
-    text,
-  },
-  (err, response) => {
-    try {
-      console.log(`Recieved from server ${JSON.stringify(response)}`);
-    } catch (error) {
-      console.log(err);
-    }
+const callback = (err, response) => {
+  try {
+    console.log(`Recieved from server ${JSON.stringify(response)}`);
+  } catch (error) {
+    console.error(err);
   }
-);
+};
+
+const tests = [
+  {
+    test: "TSVH",
+    data: {
+      table: [
+        { column: [5245.2, 45.124, 234.23, 43.5, 41.45] },
+        { column: [2525.3, 5435.4, 2462.2, 42.4, 11.35] },
+        { column: [5342.3, 43.674, 264.56, 23.4, 23.56] },
+      ],
+    },
+  },
+  {
+    test: "TASVH",
+    data: {
+      table: [
+        { column: [5245.2, 45.124, 234.23, 43.5, 41.2] },
+        { column: [2525.3, 5435.4, 2462.2, 42.4, 11.5] },
+        { column: [5342.3, 43.674, 264.56, 23.4, 23.8] },
+      ],
+    },
+  },
+  {
+    test: "TSMH",
+    data: {
+      table: [
+        { column: [5245.2, 45.124, 234.23, 43.5, 41.34] },
+        { column: [2525.3, 5435.4, 2462.2, 42.4, 11.23] },
+        { column: [5342.3, 43.674, 264.56, 23.4, 23.65] },
+      ],
+    },
+  },
+];
+
+client.octave({ tests }, callback);
